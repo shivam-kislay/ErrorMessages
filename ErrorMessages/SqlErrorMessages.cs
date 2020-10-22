bd807ebc6340eff607305cf62101abfa0c35b17f
Shivam Kislay <shivam.kislay@iloy-group.com>
2020-10-22 15:02:28 +0100

    Add project files.

---
+++ ErrorMessages/SqlErrorMessages.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ErrorMessages
+{
+    public class SqlErrorMessages
+    {
+        private static Dictionary<int, string> ErrorMessages = new Dictionary<int, string>() 
+        {
+            {547, "Error Message"}
+        };
+
+        public string getErrorMessage(int code) 
+        {
+            return ErrorMessages[code];
+        }
+    }
+}
